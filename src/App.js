d5d1eca577d438c70046ea6afafa34fae3dd832c
Ricardo Wu <ricardowu1112@gmail.com>
2023-09-30 15:10:19 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -1,44 +1,37 @@
 import React from 'react';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-//Code to import Budget.js
-import Budget from './components/Budget';
-
-// Add code to import the other components here under
 
 
 import { AppProvider } from './context/AppContext';
+import CartValue from './components/CartValue';
+import ExpenseList from './components/ExpenseList';
+import ItemSelected from './components/ItemSelected';
+import Location from './components/Location';
+
 const App = () => {
     return (
         <AppProvider>
             <div className='container'>
-                <h1 className='mt-3'>Company's Budget Allocation</h1>
-                    <div className='row mt-3'>
-                        {
-                            /* Add Budget component here */
-                        }        
-
-                        {
-                            /* Add Remaining component here*/
-                        }        
-
-                        {
-                            /* Add ExpenseTotal component here */
-                        }        
-                       
-                        {
-                            /* Add ExpenseList component here */
-                        }         
-
-                        {
-                            /* Add ExpenseItem component here */
-                        }        
-
-                        {
-                            /* Add AllocationForm component here under */
-                        }        
-
+                <h1 className='mt-3'>Shopping App</h1>
+                <div className='row mt-3'> 
+                    <div className='col-sm'>
+                        <CartValue />
+                    </div>
+                    <div className='col-sm'>
+                        <Location />
+                    </div>
+                </div>
+                <h3 className='mt-3'>Shopping Cart</h3>
+                <div className='row '>
+                    <div className='col-sm'>
+                        <ExpenseList />
+                    </div>
+                </div>
+                <h3 className='mt-3'>Add Items</h3>
+                <div className='row mt-3'>
+                    <div className='col-sm'>
+                        <ItemSelected/>
+                    </div>
                 </div>
             </div>
         </AppProvider>
